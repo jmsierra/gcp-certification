db52f4d39c82fd62b3e2f6f44afb060eeea7b22d
Juan M. Sierra <2817514+jmsierra@users.noreply.github.com>
2022-11-01 11:56:12 +0000

    Add network

--- terraform-challenge/main.tf
+++ terraform-challenge/main.tf
@@ -3,6 +3,13 @@ provider "google" {
   region      = var.region
 }
 
+terraform {
+  backend "gcs" {
+    bucket  = "tf-bucket-912370"
+    prefix  = "terraform/state"
+  }
+}
+
 module "instances" {
   source = "./modules/instances"
   machine_type = var.machine_type
@@ -12,9 +19,21 @@ module "storage" {
   source = "./modules/storage"
 }
 
-terraform {
-  backend "gcs" {
-    bucket  = "tf-bucket-912370"
-    prefix  = "terraform/state"
-  }
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "3.4.0"
+  # insert the 3 required variables here
+  project_id   = var.project_id
+  network_name = "tf-vpc-712718"
+  subnets = [
+    {
+      subnet_name           = "subnet-01"
+      subnet_ip             = "10.10.10.0/24"
+      subnet_region         = "us-east1"
+    },
+    {
+      subnet_name           = "subnet-02"
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = "us-east1"
+    }]
 }
